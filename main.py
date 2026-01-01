7c310bdf1b57502479fabce938200ef96c8e743a
Nachsus <polodono@gmail.com>
2026-01-01 23:17:05 +0100

    added so you can write the prompt while running the file in the terminal

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+
+parser = argparse.ArgumentParser(description="ai-agent")
+parser.add_argument('user_prompt', type=str, help="User prompt")
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,7 +14,7 @@ if not api_key:
 
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
 
 if not response or not response.usage_metadata:
     raise RuntimeError("response seems to have failed")
