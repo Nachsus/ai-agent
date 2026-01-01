f2ba83d358a39801fbcf8171e9645d24cc868f0b
Nachsus <polodono@gmail.com>
2026-01-01 23:26:33 +0100

    start of message history, and added verbose option for extra printing

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
 
 parser = argparse.ArgumentParser(description="ai-agent")
 parser.add_argument('user_prompt', type=str, help="User prompt")
+parser.add_argument('--verbose', action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,11 +18,14 @@ if not api_key:
 
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
 
 if not response or not response.usage_metadata:
     raise RuntimeError("response seems to have failed")
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
 print(response.text)
